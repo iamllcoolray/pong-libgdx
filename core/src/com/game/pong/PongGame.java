ed760f78c55d775abd8be22f4817f4d450dc0fad
iamllcoolray <llcoolray24@gmail.com>
2023-05-24 21:56:55 -0400

    removed unused imports

--- core/src/com/game/pong/PongGame.java
+++ core/src/com/game/pong/PongGame.java
@@ -3,7 +3,6 @@ package com.game.pong;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
